0a4abe033c192e00d731fe9279e8ef7b200aa37e
stas <chebaevs@gmail.com>
2022-08-19 22:36:02 +0500

    Задача 6

---
+++ Seminar_1/task_6/Program.cs
@@ -0,0 +1,44 @@
+﻿int number = 0;
+bool flag = true;
+
+while (flag)
+{
+    Console.Write($"Введите число: ");
+    string str = Console.ReadLine();
+ 
+   
+        if (Proverka(str) == "Число")
+        {
+            number = int.Parse(str);
+            if ((number % 2) == 0)
+            {
+                Console.WriteLine($"{number} -> да");
+                
+            }
+            else
+            {
+               Console.WriteLine($"{number} -> нет"); 
+            }
+        flag = false;
+        }
+        else
+        {
+            Console.Clear();
+        }
+}
+
+
+static string Proverka(string str)
+{
+    try
+    {
+        Convert.ToInt32(str);
+        return "Число";
+    }
+    catch (Exception)
+    {
+        return "Не число";
+    }
+
+}
+
