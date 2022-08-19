550dd0f85c665942a15ed966659e0c211f632a0d
stas <chebaevs@gmail.com>
2022-08-19 22:53:37 +0500

    Задача 8

---
+++ Seminar_1/task_8/Program.cs
@@ -0,0 +1,47 @@
+﻿int number = 0, N = 2;
+bool flag = true;
+string summa = "", temp = "";
+
+while (flag)
+{
+    Console.Write($"Введите число: ");
+    string str = Console.ReadLine();
+
+
+    if (Proverka(str) == "Число")
+    {
+        number = int.Parse(str);
+        while (N <= number)
+        {
+            if ((N % 2) == 0)
+            {
+                temp = Convert.ToString(N);
+                summa = summa + $"{temp},";
+                
+            }
+            N++;
+        }
+        flag = false;
+        Console.WriteLine($"{number} -> {summa}");
+    }
+    else
+    {
+        Console.Clear();
+    }
+}
+
+
+static string Proverka(string str)
+{
+    try
+    {
+        Convert.ToInt32(str);
+        return "Число";
+    }
+    catch (Exception)
+    {
+        return "Не число";
+    }
+
+}
+
