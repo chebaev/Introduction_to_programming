84d0319e022476f7fe1544c7c73eaa58b4045242
stas <chebaevs@gmail.com>
2022-08-21 14:25:32 +0500

    Задача 13

---
+++ Seminar_2/task_13/Program.cs
@@ -0,0 +1,44 @@
+﻿string Proverka(string str)
+{
+    try
+    {
+        Convert.ToInt32(str);
+        return "Число";
+    }
+    catch (Exception)
+    {
+        return "Не число";
+    }
+
+}
+
+bool flag = true;
+while (flag)
+{
+    Console.WriteLine("Введите число: ");
+    string number = Console.ReadLine();
+
+    if (Proverka(number) == "Число")
+    {
+        if (number.Length > 2)
+        {
+            Console.WriteLine($"{number} -> {number[2]}");
+            flag = false;
+        }
+        else
+        {
+            Console.WriteLine($"{number} -> Третьей цифры нет");
+            flag = false;
+        }
+
+
+    }
+    else
+    {
+        Console.Clear();
+    }
+
+}
+
+
+
