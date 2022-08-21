7539ab1731608bc7b4ce7e53aa08c37627c2437b
stas <chebaevs@gmail.com>
2022-08-21 14:13:43 +0500

    Задача 10

---
+++ Seminar_2/task_10/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+string Proverka(string str)
+{
+    try
+    {
+        Convert.ToInt32(str);
+        return "Число";
+    }
+    catch (Exception)
+    {
+        return "Не число";
+    }
+
+}
+
+bool flag = true;
+while (flag)
+{
+    Console.WriteLine("Введите трёхзначное число: ");
+    string number = Console.ReadLine();
+
+    if (Proverka(number) == "Число")
+    {
+
+        Console.WriteLine($"{number} -> {number[1]}");
+        flag = false;
+    }
+    else
+    {
+        Console.Clear();
+    }
+
+}
+
+
+
