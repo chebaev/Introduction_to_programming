ad7480b8b6655b3b51180eaaa4dc9a0c1bc47081
stas <chebaevs@gmail.com>
2022-08-23 09:55:22 +0500

    Задача 19

---
+++ Seminar_3/task_19/Program.cs
@@ -0,0 +1,43 @@
+﻿int Coup(int temp)
+{
+    int newNumber = 0;
+    while (temp > 0)
+    {
+        int last = temp % 10;
+        newNumber = newNumber * 10 + last;
+        temp = temp / 10;
+    }
+    return newNumber;
+}
+int Proverka()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        string str = Console.ReadLine();
+
+        try
+        {
+            number = Convert.ToInt32(str);
+            return number;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Ошибка! Неверное число. ");
+        }
+    }
+}
+
+int number = Proverka();
+int newNumber = Coup(number);
+
+if (newNumber == number)
+{
+    Console.WriteLine($"{number} -> да");
+}
+else
+{
+    Console.WriteLine($"{number} -> нет");
+}
+
