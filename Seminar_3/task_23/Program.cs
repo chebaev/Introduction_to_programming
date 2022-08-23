2015befe40e55645bdae457ff6f61ea759cfe079
stas <chebaevs@gmail.com>
2022-08-23 14:10:13 +0500

    Задача 23

---
+++ Seminar_3/task_23/Program.cs
@@ -0,0 +1,33 @@
+﻿int Proverka()
+{
+    int number;
+    while (true)
+    {
+        Console.Write("Введите число: ");
+        string str = Console.ReadLine();
+
+        try
+        {
+            number = Convert.ToInt32(str);
+            return number;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Ошибка! Неверное число. ");
+        }
+    }
+}
+
+
+//Console.WriteLine("Ввидите чсло");
+int N = Proverka();//Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+string temp = "";
+while(count <= N)
+{
+    temp = temp +' '+ Math.Pow(count, 3);
+    
+    count++;      
+}
+Console.WriteLine($"{N} ->  {temp} "); 
