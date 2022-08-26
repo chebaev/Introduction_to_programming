6414a58de18d6ba41b4d985705089a531299f588
stas <chebaevs@gmail.com>
2022-08-26 11:36:29 +0500

    Задача 27

---
+++ Seminar_4/task_27/Program.cs
@@ -0,0 +1,27 @@
+﻿int Proverka()
+{
+    int number;
+    while (true)
+    {
+        Console.Write($"Введите число: ");
+        string str = Console.ReadLine();
+
+        try
+        {
+            number = Convert.ToInt32(str);
+            return number;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Ошибка! Неверное число. ");
+        }
+    }
+}
+
+int number = Proverka();
+int summa = 0;
+
+foreach (char c in Math.Abs(number).ToString())
+        summa += c - '0';
+Console.WriteLine($"{number} -> {summa}");
+
