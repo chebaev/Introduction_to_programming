1ce89243b305273376aa2ed9600b1ab2b675e067
stas <chebaevs@gmail.com>
2022-08-26 14:26:33 +0500

    Задача 29

---
+++ Seminar_4/task_29/Program.cs
@@ -0,0 +1,45 @@
+﻿string Proverka(string message)
+{
+
+    while (true)
+    {
+        Console.Write(message);
+        string str = Console.ReadLine();
+        string[] temp = str.Split(',');
+
+        for (int i = 0; i < temp.Length; i++)
+        {
+            try
+            {
+                Convert.ToInt32(temp[i]);
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка! Неверное число. ");
+            }
+        }
+        return str;
+    }
+}
+
+string text = "Введите массив чисел через запетую (,): ";
+string str = Proverka(text);
+int[] array = str.Split(',').Select(int.Parse).ToArray();
+string temp = "";
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (temp == "")
+    {
+        temp = Convert.ToString(array[i]);
+    }
+    else
+    {
+        temp = temp + ", " + Convert.ToString(array[i]);
+    }
+}
+
+Console.WriteLine($"{temp} -> [{temp}]");
+
+
