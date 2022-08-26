a34cba0ab0fa1592a550ccaca5222ff3a5147e5a
stas <chebaevs@gmail.com>
2022-08-26 10:15:01 +0500

    Задача 25

---
+++ Seminar_4/task_25/Program.cs
@@ -0,0 +1,30 @@
+﻿int Proverka(string letter)
+{
+    int number;
+    while (true)
+    {
+        Console.Write($"Введите число {letter}: ");
+        string str = Console.ReadLine();
+
+        try
+        {
+            number = Convert.ToInt32(str);
+            return number;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Ошибка! Неверное число. ");
+        }
+    }
+}
+
+int A = Proverka("A");
+int B = Proverka("B");
+int summa = 1;
+Console.WriteLine($"Проверка = {Math.Pow(A, B)}");
+
+for (int i = 0; i < B; i++)
+{
+    summa = summa * A;
+}
+Console.WriteLine($"{A}, {B} -> {summa}");
