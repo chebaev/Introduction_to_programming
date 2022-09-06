1ac425f07dd7992f6bad49fd10dcc7b5e28b3e6a
stas <chebaevs@gmail.com>
2022-09-06 12:43:02 +0500

    Задача 41

--- Seminar_6/task_41/Program.cs
+++ Seminar_6/task_41/Program.cs
@@ -1,55 +1,16 @@
-﻿
-// Функция проверки массива на число
-// string Proverka(string message)
-// {
-
-//     while (true)
-//     {
-//         Console.Write(message);
-//         string str = Console.ReadLine();
-//         string[] temp = str.Split(',');
-
-//         for (int i = 0; i < temp.Length; i++)
-//         {
-//             try
-//             {
-//                 Convert.ToInt32(temp[i]);
-
-//             }
-//             catch (Exception)
-//             {
-//                 Console.WriteLine("Ошибка! Неверное число. ");
-//             }
-//         }
-//         return str;
-//     }
-// }
-
-// int AmountOfNumbers()
-// {
-//     pass;
-// }
-
-// string text = "Введите массив чисел через запетую (,): ";
-// string str = Proverka(text);
-// int[] array = str.Split(',').Select(int.Parse).ToArray();
-
-//Console.WriteLine("Сколько чисел будете проверять. Введите число: ");
-// int number = Console.ReadLine();
-// bool isNum = double.TryParse()
-
-
+﻿//Функция проверки массива на число
 bool CheckingForNumber(string str)
 {
     int num;
-    bool isNum = int.TryParse(str, out num);
-    return isNum;
+    return int.TryParse(str, out num);
 }
+
+//Фукция вывода сообщений и получения числа
 int GetData(string message)
 {
-    while(true)
-    { 
-        Console.Write(message); 
+    while (true)
+    {
+        Console.Write(message);
         string number = Console.ReadLine();
         if (CheckingForNumber(number))
         {
@@ -59,25 +20,25 @@ int GetData(string message)
         else
         {
             Console.WriteLine("Введено не число. Попробуйте ещё раз.");
+            Console.WriteLine();
         }
     }
 }
-
+//Функция вывода массива и подсчёта сколько чисел больше 0
 void HowManyNumbers(int num)
 {
     int count = 0;
     int[] temp = new int[num];
-    for (int i = 1; i<=num; i++)
+    for (int i = 1; i <= num; i++)
     {
         int number = GetData($"Введите {i} число: ");
-        temp[i-1] = number;
+        temp[i - 1] = number;
         if (number > 0)
         {
             count++;
         }
     }
     Console.WriteLine($" {string.Join(", ", temp)} -> {count}");
-      
 }
 
 string messag = "Сколько чисел будете проверять. Введите число: ";
